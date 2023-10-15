df1fe78723af268df41ced846c12299521368495
clementmarius <marius-44620@hotmail.fr>
2023-10-15 20:02:52 +0200

    1st commit

--- eni_demo_1/audio.js
+++ eni_demo_1/audio.js
@@ -1,4 +1,12 @@
+// Tracks 1 : 
+
+
+// Récupération id du bouton
+
 document.getElementById('play-btn').addEventListener('click', function() {
+
+    // Récupération id audio
+
     var audio = document.getElementById('audio-player');
     if (audio.paused) {
       audio.play();
@@ -8,3 +16,45 @@ document.getElementById('play-btn').addEventListener('click', function() {
       this.innerHTML = 'Lire la musique'; // Mettez le texte "Lire la musique" sur le bouton
     }
   });
+
+
+// Tracks 2 : 
+
+  document.getElementById('play-btn-second').addEventListener('click', function() {
+    var audio = document.getElementById('audio-player-second');
+    if (audio.paused) {
+      audio.play();
+      this.innerHTML = 'Pause'; // Mettez le texte "Pause" sur le bouton
+    } else {
+      audio.pause();
+      this.innerHTML = 'Lire la musique'; // Mettez le texte "Lire la musique" sur le bouton
+    }
+  }); 
+
+
+// Tracks 3 : 
+
+  document.getElementById('play-btn-third').addEventListener('click', function() {
+    var audio = document.getElementById('audio-player-third');
+    if (audio.paused) {
+      audio.play();
+      this.innerHTML = 'Pause'; // Mettez le texte "Pause" sur le bouton
+    } else {
+      audio.pause();
+      this.innerHTML = 'Lire la musique'; // Mettez le texte "Lire la musique" sur le bouton
+    }
+  }); 
+
+
+//  Tracks 4 : 
+
+document.getElementById('play-btn-fourth').addEventListener('click', function() {
+    var audio = document.getElementById('audio-player-fourth');
+    if (audio.paused) {
+      audio.play();
+      this.innerHTML = '<i class="fa fa-play"'; // Mettez le texte "Pause" sur le bouton
+    } else {
+      audio.pause();
+      this.innerHTML = 'Lire la musique'; // Mettez le texte "Lire la musique" sur le bouton
+    }
+  }); 
